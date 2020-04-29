be04d23ce02d680a19611c1ac2a961eada20a70f
Derien Stephens <derienstephens@Deriens-MacBook-Pro.local>
2020-04-29 11:46:49 -0400

    Implemented Papa parse and can get csv data in console

--- EmailServiceApp/ClientApp/src/App.js
+++ EmailServiceApp/ClientApp/src/App.js
@@ -5,6 +5,8 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { Email } from './components/Email';
+import { Dashboard } from './components/Dashboard';
+
 
 
 import './custom.css'
@@ -19,6 +21,7 @@ export default class App extends Component {
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
         <Route path='/compose' component={Email} />
+        <Route path='/dashboard' component={Dashboard} />
 
       </Layout>
     );
---
+++ EmailServiceApp/ClientApp/src/components/Dashboard.js
@@ -0,0 +1,48 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Papa from 'papaparse';
+
+
+export class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            subscribers: []
+        }
+
+        this.updateData = this.updateData.bind(this);
+
+    }
+    componentDidMount() {
+        console.log("working");
+
+        
+        Papa.parse('./subscribers.csv', {
+            header: true,
+            delimiter: ',',
+            download: true,
+            skipEmptyLines: false,
+            complete: this.updateData
+
+        });
+    }
+
+    updateData(result) {
+        console.log("getting called");
+        console.log(result);
+
+        this.setState({ subscribers: result });
+    }
+
+    render() {
+
+        console.log(this.state);
+
+        return (
+            <h1>Dashboard</h1>
+        );
+    }
+}
+
+export default Dashboard;
